7d7a5e52712152f203126ab2d70f2dca3d156e19
Peter Bozsoky <peter-bozsoky@student.kit.edu>
2017-10-26 21:56:16 +0200

    Removed unused imports, fixed formatting for pep8 compatibility.

--- filetypes.py
+++ filetypes.py
@@ -1,16 +1,21 @@
 from collections import namedtuple
 
-import mutagen
 from mutagen.mp3 import MP3
 from mutagen.mp3 import BitrateMode
 from mutagen.oggvorbis import OggVorbis
 from mutagen.easyid3 import EasyID3
 from mutagen.flac import FLAC
 
-MusicTag = namedtuple("MusicTag", ["title", "title_number", "album", "artist", "album_artist"])
-MusicFile = namedtuple("MusicFile", ["path", "file", "bitrate", "tag"])
+MusicTag = namedtuple(
+    "MusicTag",
+    ["title", "title_number", "album", "artist", "album_artist"]
+)
+MusicFile = namedtuple(
+    "MusicFile",
+    ["path", "file", "bitrate", "tag"]
+)
 
-types = dict();
+types = dict()
 
 types["mp3"] = dict()
 types["mp3"]["ext"] = ".mp3"
@@ -27,7 +32,7 @@ types["ogg"]["dir"] = "OGG"
 
 def get_file_type(f: str):
     out_type = None
-    
+
     for t in types:
         if f.endswith(types[t]["ext"]):
             out_type = t
@@ -43,15 +48,15 @@ def get_file_bitrate(f):
             bitrate = -1
         else:
             bitrate = MP3(f).info.bitrate
-    
+
     elif f.endswith(types["ogg"]["ext"]):
         bitrate = OggVorbis(f).info.bitrate
-    
+
     return bitrate
 
 
 def get_file_tag(f):
-    system_tag= None
+    system_tag = None
 
     if f.endswith(types["mp3"]["ext"]):
         system_tag = EasyID3(f)
@@ -61,9 +66,10 @@ def get_file_tag(f):
         system_tag = OggVorbis(f)
 
     out_tag = MusicTag(system_tag["title"][0],
-                   system_tag["tracknumber"][0],
-                   system_tag["album"][0],
-                   system_tag["artist"][0],
-                   system_tag["albumartist"][0])
-    
+                       system_tag["tracknumber"][0],
+                       system_tag["album"][0],
+                       system_tag["artist"][0],
+                       system_tag["albumartist"][0]
+                       )
+
     return out_tag
--- musicsort.py
+++ musicsort.py
@@ -1,9 +1,9 @@
-from os import listdir, walk, makedirs, symlink
-from os.path import isfile, join, abspath
+from os import walk, makedirs, symlink
+from os.path import join, abspath
 from shutil import copyfile
 from typing import List
 from filetypes import types, get_file_type, get_file_bitrate, get_file_tag
-from filetypes import MusicFile, MusicTag
+from filetypes import MusicFile
 import argparse
 
 MusicFileList = List[MusicFile]
@@ -14,17 +14,39 @@ def bitrate_is_valid(bitrate, threshold):
 
 
 def main():
-    parser = argparse.ArgumentParser(description="""Sorts and filters a given directory of music 
-                                                    files by filetype and bitrate (in case of lossy 
-                                                    compression).""")
-    parser.add_argument("-dir", "--directory", help="Path to the music files.", default=".")
-    parser.add_argument("-out", "--output_directory", help="""Path to where the tree of sorted 
-                                                              files shall root.""", default="out")
-    parser.add_argument("-mbr", "--min_bitrate", default="0", help="""The minimum bitrate (in kBit/s) for 
-                                                                      lossily-compressed files.""")
-    parser.add_argument("-sim", "--simulate", action="store_true")
-    parser.add_argument("-bug", "--debug", action="store_true")
-    
+    parser = argparse.ArgumentParser(
+        description="Sorts and filters a given directory of music files by "
+                    "filetype and bitrate (in case of lossy compression)."
+    )
+    parser.add_argument(
+        "-dir",
+        "--directory",
+        help="Path to the music files.",
+        default="."
+    )
+    parser.add_argument(
+        "-out",
+        "--output_directory",
+        help="Path to where the tree of sorted files shall root.",
+        default="out"
+    )
+    parser.add_argument(
+        "-mbr",
+        "--min_bitrate",
+        default="0",
+        help="The minimum bitrate (in kBit/s) for lossily-compressed files."
+    )
+    parser.add_argument(
+        "-sim",
+        "--simulate",
+        action="store_true"
+    )
+    parser.add_argument(
+        "-bug",
+        "--debug",
+        action="store_true"
+    )
+
     args = parser.parse_args()
     print(args)
 
@@ -38,9 +60,18 @@ def main():
     bugprint(all_files, debug)
     bugprint("=========", debug)
     music_files = filter_music_files(all_files)
-    good_music_files = filter_high_bitrate_music_files(music_files, int(args.min_bitrate))
+    good_music_files = filter_high_bitrate_music_files(
+        files=music_files,
+        min_bitrate=int(args.min_bitrate)
+    )
     sorted_music_files = sort_music_files(good_music_files)
-    write_sorted_files(output_dir, sorted_music_files, debug=debug, simulate=simulate, all_files=music_files)
+    write_sorted_files(
+        output_dir=output_dir,
+        filey_by_type=sorted_music_files,
+        debug=debug,
+        simulate=simulate,
+        all_files=music_files
+    )
 
 
 def bugprint(s, debug):
@@ -62,12 +93,14 @@ def sort_music_files(files: MusicFileList):
 
 
 def filter_high_bitrate_music_files(files: MusicFileList, min_bitrate):
-    return [file for file in files if bitrate_is_valid(file.bitrate, min_bitrate)]
+    return [file for file
+            in files
+            if bitrate_is_valid(file.bitrate, min_bitrate)]
 
 
 def gather_files(root, debug=False):
     all_files = []
-    for (dirpath, dirnames, filenames) in walk(root):
+    for (dirpath, _, filenames) in walk(root):
         for name in filenames:
             if get_file_type(join(dirpath, name)):
                 bugprint(name, debug)
@@ -90,7 +123,8 @@ def filter_music_files(files: list):
     return music_files
 
 
-def write_sorted_files(output_dir, files_by_type, debug=False, simulate=False, all_files=None):
+def write_sorted_files(output_dir, files_by_type,
+                       debug=False, simulate=False, all_files=None):
     if debug:
         print("*ALL* \t> " + repr(all_files))
     for t in files_by_type:
@@ -101,7 +135,8 @@ def write_sorted_files(output_dir, files_by_type, debug=False, simulate=False, a
 
 
 def copy_file_list(path: str, musicfiles: MusicFileList, simulate=False):
-    """Copies a given list of files to the given path (if simulate is False), or creates symlinks instead (if simulate is True)."""
+    "Copies a given list of files to the given path (if simulate is False), "
+    "or creates symlinks instead (if simulate is True)."
     for mf in musicfiles:
         if simulate:
             copyfile(mf.path, join(path, mf.file))
